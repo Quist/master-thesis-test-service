bbba3210c48d1f305a942e9279b08df5689429a1
quist <joakiml@gmail.com>
2016-01-11 12:05:45 +0100

    Produce a runnable jar.

--- src/main/java/example/HelloWorld.java
+++ src/main/java/example/HelloWorld.java
@@ -1,4 +1,5 @@
 package example;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
@@ -6,10 +7,26 @@ import javax.xml.ws.Endpoint;
 @WebService
 public class HelloWorld {
 
-  @WebMethod
-  public String sayHelloWorldFrom(String from) {
-    String result = "Hello, world, from " + from;
-    System.out.println(result);
-    return result;
-  }
+    public static void main(String args[]) {
+        if (args.length < 1) {
+            printUsage();
+            System.exit(1);
+        }
+
+        String port = args[0];
+        String address = "http://localhost:" + port + "/ws/hello";
+        Endpoint.publish(address, new HelloWorld());
+        System.out.println("Service listening on address " + address);
+    }
+
+    static void printUsage(){
+        System.out.println("usage: HelloWorld port");
+    }
+
+    @WebMethod
+    public String sayHelloWorldFrom(String from) {
+        String result = "Hello, world, from " + from;
+        System.out.println(result);
+        return result;
+    }
 }
